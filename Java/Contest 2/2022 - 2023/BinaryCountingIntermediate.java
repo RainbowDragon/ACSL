33d7004fac13609a132020529fd85cab097b1a53
Xiang Xuan <xuanxiang@hotmail.com>
2023-12-26 12:12:31 -0800

    ACSL 2022-2023 - Contest 2 - Binary Counting - Intermediate Division

---
+++ Java/Contest 2/2022 - 2023/BinaryCountingIntermediate.java
@@ -0,0 +1,121 @@
+/**
+ *      ACSL 2022-2023 - Contest 2 - Binary Counting - Intermediate Division
+ *
+ */
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class BinaryCountingIntermediate {
+
+    /*
+     * Complete the 'findLastBinary' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+    static int findLastBinary (String s) {
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++)
+        {
+            String charBinaryString = getBinaryStringForChar(s.charAt(i));
+            sb.append(charBinaryString);
+        }
+        String binaryString = sb.toString();
+
+        int number = 0;
+        boolean found = false;
+        while (!found)
+        {
+            String numberString = getBinaryStringForInt(number);
+            int length = numberString.length();
+
+            int first = binaryString.indexOf(numberString);
+            if (first != -1) {
+                binaryString = binaryString.substring(0, first) + binaryString.substring(first+length);
+            }
+
+            int last = binaryString.lastIndexOf(numberString);
+            if (last != -1) {
+                binaryString = binaryString.substring(0, last) + binaryString.substring(last+length);
+            }
+
+            if (first == -1 && last == -1) {
+                found = true;
+                number--;
+            }
+            else {
+                number++;
+            }
+        }
+
+        return number;
+    }
+
+    static String getBinaryStringForChar (char c) {
+
+        char[] result = new char[8];
+        Arrays.fill(result, '0');
+
+        int number = (int)c;
+        int index = 7;
+        while (number > 0)
+        {
+            result[index] = (number % 2 == 0) ? '0' : '1';
+            number /= 2;
+            index--;
+        }
+
+        return new String(result);
+    }
+
+    static String getBinaryStringForInt (int number) {
+
+        if (number == 0) {
+            return "0";
+        }
+
+        String result = "";
+        while (number > 0)
+        {
+            int remainder = number % 2;
+            number /= 2;
+            result = remainder + result;
+        }
+
+        return result;
+    }
+
+    public static void main (String [] args) {
+
+        String[] input = new String[10];
+        input[0] = "Roses are red.";
+        input[1] = "A is Alpha; B is Bravo; C is Charlie.";
+        input[2] = "A stitch in time saves nine.";
+        input[3] = "1, 2: Buckle my shoe! 3, 4: Shut the door!";
+        input[4] = "Is HackerRank the platform used by ACSL?";
+        input[5] = "The quick brown fox jumped over the lazy dogs.";
+        input[6] = "ACSL is 45 years old and going strong.";
+        input[7] = "What was the first computer programming language you learned?";
+        input[8] = "Supercalifragilisticexpialidocious!";
+        input[9] = "Lions and Tigers and Bears, Oh My! This is from The Wizard of Oz.";
+
+        int[] output = {12, 20, 14, 22, 27, 29, 27, 30, 20, 26};
+
+        for (int i = 0; i < 10; i++)
+        {
+            int result = findLastBinary(input[i]);
+
+            if (output[i] == result) {
+                System.out.println("Test Case " + i + ": Passed!");
+            }
+            else {
+                System.out.println("Test Case " + i + ": Failed!");
+                System.out.println("Expected output: " + output[i]);
+                System.out.println("Your output: " + result);
+            }
+        }
+    }
+}
