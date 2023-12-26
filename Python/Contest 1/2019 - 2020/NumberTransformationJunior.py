5d9e4cb4114300987b294759d7fe4731592494f7
Xiang Xuan <xuanxiang@hotmail.com>
2023-12-25 21:03:20 -0800

    ACSL 2019-2020 - Contest 1 - Number Transformation - Junior Division

---
+++ Python/Contest 1/2019 - 2020/NumberTransformationJunior.py
@@ -0,0 +1,54 @@
+#
+# ACSL 2019-2020 - Contest 1 - Number Transformation - Junior Division
+#
+#
+
+def transformNumber(num, pos, delta):
+
+    result = 0
+
+    digits = [0] * 20
+    index = 0
+    while num > 0:
+        digits[index] = num % 10
+        num //= 10
+        index += 1
+
+    pos -= 1
+    digit = digits[pos]
+    if digit < 5:
+        digit += delta
+        digit %= 10
+    else:
+        digit -= delta
+        if digit < 0:
+            digit *= -1
+
+        while digit > 9:
+            digit //= 10
+
+    digits[pos] = digit
+
+    for k in range(pos):
+        digits[k] = 0
+
+    base = 1
+    for k in range(index):
+        result += digits[k] * base
+        base *= 10
+
+    return result
+
+
+test_input = [[124987, 2, 3], [540670, 3, 9], [7145042, 2, 8], [124987, 2, 523], [4386709, 1, 2], [4318762, 4, 3], [72431685, 1, 7], [123456789, 7, 8], [9876543210, 10, 25], [314159265358, 8, 428]]
+test_output = [124950, 540300, 7145020, 124950, 4386707, 4315000, 72431682, 121000000, 1000000000, 314140000000]
+
+for i in range(10):
+    test_result = transformNumber(test_input[i][0], test_input[i][1], test_input[i][2])
+
+    if test_result == test_output[i]:
+        print("Test Case " + str(i) + ": Passed!")
+    else:
+        print("Test Case " + str(i) + ": Failed!")
+        print("Expected output: " + str(test_output[i]))
+        print("Your output: " + str(test_result))
