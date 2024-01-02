6e86f48d81a5aa2ebe401fec8f7e562a747b9f46
Xiang Xuan <xuanxiang@hotmail.com>
2024-01-02 12:34:53 -0800

    ACSL 2022-2023 - Contest 1 - Next Base - Senior Division

--- Python/Contest 1/2022 - 2023/NextBaseSenior.py
+++ Python/Contest 1/2022 - 2023/NextBaseSenior.py
@@ -16,9 +16,10 @@ def findModeCount(num, base, start):
 
     result = 0
     counts = [0] * base
+    start_value = int(start, base)
 
     for k in range(num):
-        number = start + k
+        number = start_value + k
 
         while number > 0:
             digit = number % base
@@ -32,7 +33,7 @@ def findModeCount(num, base, start):
     return result
 
 
-test_input = [[15, 8, 2], [25, 2, 123], [20, 12, 1427], [10, 16, 11259375], [1000, 2, 1], [50, 4, 6], [75, 9, 319], [500, 14, 27212], [700, 11, 14630], [25, 2, 3130]]
+test_input = [[15, 8, "2"], [25, 2, "1111011"], [20, 12, "9AB"], [10, 16, "ABCDEF"], [1000, 2, "1"], [50, 4, "12"], [75, 9, "384"], [500, 14, "9CBA"], [700, 11, "AAA0"], [25, 2, "110000111010"]]
 test_output = [9, 105, 14, 10, 4938, 42, 88, 336, 940, 165]
 
 for i in range(10):
@@ -44,3 +45,4 @@ for i in range(10):
         print("Test Case " + str(i) + ": Failed!")
         print("Expected output: " + str(test_output[i]))
         print("Your output: " + str(test_result))
+
