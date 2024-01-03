f7a61031ef17419cd40397ee614b441a58746107
Xiang Xuan <xuanxiang@hotmail.com>
2024-01-02 16:13:30 -0800

    ACSL 2020-2021 - Contest 1 - Numeral Triangles - Intermediate Division

--- Python/Contest 1/2020 - 2021/NumeralTrianglesIntermediate.py
+++ Python/Contest 1/2020 - 2021/NumeralTrianglesIntermediate.py
@@ -51,7 +51,18 @@ def sumOfOctDigit(number):
     return digit_sum
 
 
-test_input = [[2, 3, 5], [221, 2, 4], [1, 4, 20], [10, 10, 10], [3245, 5, 11], [4567, 7, 65], [3141, 5, 26], [765, 43, 21], [704, 1776, 20], [77, 7, 100]]
+test_input = [[0] * 3] * 10
+test_input[0] = [2, 3, 5]
+test_input[1] = [221, 2, 4]
+test_input[2] = [1, 4, 20]
+test_input[3] = [10, 10, 10]
+test_input[4] = [3245, 5, 11]
+test_input[5] = [4567, 7, 65]
+test_input[6] = [3141, 5, 26]
+test_input[7] = [765, 43, 21]
+test_input[8] = [704, 1776, 20]
+test_input[9] = [77, 7, 100]
+
 test_output = [36, 38, 230, 99, 178, 1038, 429, 329, 374, 1547]
 
 for i in range(10):
