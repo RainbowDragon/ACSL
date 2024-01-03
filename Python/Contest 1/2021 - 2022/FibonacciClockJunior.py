9b1e6cc9890551a30f89e08ff2f658d6c8678e69
Xiang Xuan <xuanxiang@hotmail.com>
2024-01-02 17:39:56 -0800

    ACSL 2021-2022 - Contest 1 - Fibonacci Clock - Junior Division

--- Python/Contest 1/2021 - 2022/FibonacciClockJunior.py
+++ Python/Contest 1/2021 - 2022/FibonacciClockJunior.py
@@ -41,7 +41,18 @@ def findTime(c1, c2, c3, c4, c5):
     return str_hour + ":" + str_minute
 
 
-test_input = [['R', 'W', 'G', 'B', 'G'], ['W', 'B', 'B', 'G', 'R'], ['B', 'G', 'B', 'B', 'R'], ['W', 'W', 'W', 'B', 'B'], ['W', 'R', 'G', 'G', 'G'], ['G', 'R', 'W', 'B', 'B'], ['R', 'B', 'B', 'W', 'G'], ['W', 'W', 'W', 'W', 'W'], ['B', 'W', 'W', 'G', 'R'], ['W', 'B', 'B', 'B', 'B']]
+test_input = [[0] * 5] * 10
+test_input[0] = ['R', 'W', 'G', 'B', 'G']
+test_input[1] = ['W', 'B', 'B', 'G', 'R']
+test_input[2] = ['B', 'G', 'B', 'B', 'R']
+test_input[3] = ['W', 'W', 'W', 'B', 'B']
+test_input[4] = ['W', 'R', 'G', 'G', 'G']
+test_input[5] = ['G', 'R', 'W', 'B', 'B']
+test_input[6] = ['R', 'B', 'B', 'W', 'G']
+test_input[7] = ['W', 'W', 'W', 'W', 'W']
+test_input[8] = ['B', 'W', 'W', 'G', 'R']
+test_input[9] = ['W', 'B', 'B', 'B', 'B']
+
 test_output = ["04:50", "08:30", "11:35", "08:40", "01:50", "09:45", "04:40", "00:00", "06:20", "11:55"]
 
 for i in range(10):
@@ -53,4 +64,3 @@ for i in range(10):
         print("Test Case " + str(i) + ": Failed!")
         print("Expected output: " + str(test_output[i]))
         print("Your output: " + str(test_result))
-
