9cc54f9a6d37cfe0bc7314da3f0c9fca6c10970c
Xiang Xuan <xuanxiang@hotmail.com>
2024-05-19 23:17:24 -0700

    ACSL 2022-2023 - Contest 1 - Next Base

--- Python/Contest 1/2022 - 2023/NextBaseSenior.py
+++ Python/Contest 1/2022 - 2023/NextBaseSenior.py
@@ -33,17 +33,10 @@ def find_mode_count(num, base, start):
     return result
 
 
-test_input = [[0] * 3] * 10
-test_input[0] = [15, 8, "2"]
-test_input[1] = [25, 2, "1111011"]
-test_input[2] = [20, 12, "9AB"]
-test_input[3] = [10, 16, "ABCDEF"]
-test_input[4] = [1000, 2, "1"]
-test_input[5] = [50, 4, "12"]
-test_input[6] = [75, 9, "384"]
-test_input[7] = [500, 14, "9CBA"]
-test_input[8] = [700, 11, "AAA0"]
-test_input[9] = [25, 2, "110000111010"]
+test_input = [
+    [15, 8, "2"], [25, 2, "1111011"], [20, 12, "9AB"], [10, 16, "ABCDEF"], [1000, 2, "1"],
+    [50, 4, "12"], [75, 9, "384"], [500, 14, "9CBA"], [700, 11, "AAA0"], [25, 2, "110000111010"]
+]
 
 test_output = [9, 105, 14, 10, 4938, 42, 88, 336, 940, 165]
 
